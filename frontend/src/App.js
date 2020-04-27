bf896a894ff6e2f3cb88a6f6f4aba3b70cecce79
tshuttles <tshuttlesworth@gmail.com>
2020-04-27 10:19:28 -0500

    run npm install and update home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Water</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        >Record How Much Your Drank Today!</a>
       </header>
     </div>
   );

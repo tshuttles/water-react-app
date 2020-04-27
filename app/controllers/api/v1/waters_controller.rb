1f7c68918a143a0a883a2036393d0618957fb13e
tshuttles <tshuttlesworth@gmail.com>
2020-04-26 20:50:03 -0500

    set up water controller

--- app/controllers/api/v1/waters_controller.rb
+++ app/controllers/api/v1/waters_controller.rb
@@ -1,2 +1,41 @@
-class WatersController < ApplicationController
+class Api::V1::WatersController < ApplicationController
+  before_action :find_water, only: [:edit, :update]
+
+  def index 
+    waters = Water.all 
+    render json: WaterSerializer.new(waters).to_serialized_json
+  end 
+
+  def create 
+    water = Water.new(water_params)
+    if water.save 
+      render json: water
+    else 
+      render json: {error: "Error! Could not input water amount."}
+    end 
+  end 
+
+  def edit
+  end
+
+  def update 
+    water.update(water_params)
+    render json: @water 
+  end 
+
+  def destroy
+    water = Water.find(params[:id])
+    water.delete
+  end
+
+  private 
+
+  def water_params
+    params.require(:water).permit(:amount)
+  end 
+
+  def find_water 
+    water = Water.find_by(id: params[:id])
+  end 
+
 end

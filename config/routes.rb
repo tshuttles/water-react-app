2787ac403af7feffe321f00014ee5fdf529c4a8d
tshuttles <tshuttlesworth@gmail.com>
2020-04-29 17:43:19 -0500

    update routes file, create and set up current user action file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/api/v1/login", to: "sessions#create"
+
   namespace :api do
     namespace :v1 do 
       resources :users
